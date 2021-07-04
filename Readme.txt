e3785e2006e67402c4a110fa230d0127359cf9a4
Gooze0100 <kalniusg@gmail.com>
2021-07-04 15:06:49 +0300

    Final changes.

--- Readme.txt
+++ Readme.txt
@@ -1,21 +1,8 @@
 Project created for IBM Internship - Javascript Application Developer position.
 1. Created a webpage using "GNews" API's (https://gnews.io) to search, list and preview articles. For the user logging actions used object and JSON type file.
-2. To run this webpage you need to use some code editor (we recommend to use Visual Code Editor). In editor use Live Server which will help you to run this webpage. 
-3. 
-4. For API we used ''GNews'' API. For more information please follow this link below: https://gnews.io/docs/v4?javascript#introduction
-5. 
-6.
-7.
-8.
-12. All information was used from GNews API 2021 Free trail, no licence used. For styles used Tailwind.
-13. My email address for the questions kalniusg@gmail.com
-
-Installation instructions
-A short example/tutorial
-Write an API documentation
-What a function do
-What the function's parameters or arguments are
-What a function returns
-An example for code documentation.
-Document your code
-Apply coding conventions, such as file organization, comments, naming conventions, programming practices, etc.
+2. To run this webpage you need to use some code editor (we recommend to use Visual Code Editor). In editor use Live Server by clicking on index.html file with right mouse button and Open with Live Server, them webpage should open in your default browser. When you will open this webpage you will nee just search.
+3. For API we used ''GNews'' API and personal code. For more information please follow this link below: https://gnews.io/docs/v4?javascript#introduction
+4. In this code there are just few most important functions for addEventListeners to fetch data, create elements, add classes for those elements and upload them to HTML file to show it to user. Other function is used to add links for articles. Functions arguments are the same events on which is this functions called.
+5. LocalStorage/bowser storage used to store search input keywords and also it is the same for articles which were clicked or opened.
+6. All information was used from GNews API 2021 Free trail, no licence used. For styles used Tailwind.
+7. My email address for the questions kalniusg@gmail.com
--- public/app.js
+++ public/app.js
@@ -17,7 +17,7 @@ input.addEventListener('input', (e) => {
 
 // Button to search============================================
 button.addEventListener('click', (event) => {
-    // Prevent window reload
+    // Prevent window reload from button============================================
     event.preventDefault();
 
     if (input.value !== '') {
@@ -28,7 +28,7 @@ button.addEventListener('click', (event) => {
             console.log(data);
             const container = document.querySelector('.container');
     
-            // Clear all created DOM
+            // Clear all created DOM============================================
             while (container.firstChild) {
                 container.removeChild(container.lastChild);
             }
@@ -38,36 +38,47 @@ button.addEventListener('click', (event) => {
                 const createCard = document.createElement('div');
                 createCard.classList.add('p-10');
                 container.append(createCard);
+
                 const createMainA = document.createElement('a');
                 createCard.append(createMainA);
+
                 const createMain = document.createElement('div');
                 createMain.classList.add('max-w-sm', 'rounded', 'overflow-hidden', 'shadow-lg');
                 createMainA.append(createMain);
+
                 const createImg = document.createElement('img');
                 createImg.classList.add('w-full');
+                createImg.setAttribute('src', data.articles[i].image);
+                createImg.setAttribute('alt', data.articles[i].description);
                 createMain.append(createImg);
-                createImg.setAttribute('src', data.articles[i].image)
-                createImg.setAttribute('alt', data.articles[i].description)
+
                 const createText = document.createElement('div');
                 createText.classList.add('px-6', 'py-4');
                 createMain.append(createText);
+
                 const createTitle = document.createElement('div');
                 createTitle.classList.add('font-bold', 'text-xl', 'mb-2');
                 createText.append(createTitle);
+
                 const createTextNodeTitle = document.createTextNode(data.articles[i].title);
                 createTitle.append(createTextNodeTitle);
+
                 const createPar = document.createElement('p');
                 createPar.classList.add('text-gray-700', 'text-base');
                 createPar.style.textOverflow = 'ellipsis';
                 createText.append(createPar);
+
                 const createTextNodePar = document.createTextNode(data.articles[i].description);
                 createPar.append(createTextNodePar);
+
                 const createSpanDiv = document.createElement('div');
                 createSpanDiv.classList.add('px-6', 'pt-4', 'pb-2');
                 createMain.append(createSpanDiv);
+
                 const createSpan = document.createElement('span');
                 createSpan.classList.add('inline-block', 'bg-gray-200', 'rounded-full', 'px-3', 'py-1', 'text-sm', 'font-semibold', 'text-gray-700', 'mr-2', 'mb-2');
                 createSpanDiv.append(createSpan);
+
                 const d = data.articles[i].publishedAt
                 const createTextNodeSpan = document.createTextNode(new Date(d).toISOString().slice(0, 16).replace('T',' '));
                 createSpan.append(createTextNodeSpan);
